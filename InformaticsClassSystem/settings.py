f6018e39bd461cf58168add7df9ba55b9349caec
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2025-03-01 07:38:56 +0300

    Added Database

--- InformaticsClassSystem/settings.py
+++ InformaticsClassSystem/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +79,9 @@ WSGI_APPLICATION = 'InformaticsClassSystem.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="postgresql://Informatics_owner:npg_Juwb7Y8KNnDj@ep-wispy-feather-a5ayq3oo-pooler.us-east-2.aws.neon.tech/Informatics?sslmode=require"
+    )
 }
 
 
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
